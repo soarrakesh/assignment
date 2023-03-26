b1c1e6eef905aedff182eab22e402824b7e29a53
Rakesh Dwivedi <soar.rakesh@gmail.com>
2023-03-26 15:02:50 +0530

    Change comment

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ import configs from './src/config/index.js';
 // Use middleware body-parser
 app.use(bodyParser.json());
 
-// Syncronizing models with database tables
+// Syncronizing models with database tables comment this line
 // models.sequelize
 //   .sync({ alter: true })
 //   .then(() => {
